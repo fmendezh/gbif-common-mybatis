9a0a8ad51895a233c643ef2cd68fe864a4a4c6fe
wixner <wixner@0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c>
2012-02-06 16:14:43 +0000

    Aslo move tests
    git-svn-id: https://gbif-common-resources.googlecode.com/svn/gbif-common-mybatis/trunk@566 0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c

---
+++ src/test/java/org/gbif/mybatis/type/IdentifierTypeConverterTest.java
@@ -0,0 +1,30 @@
+package org.gbif.mybatis.type;
+
+import org.gbif.api.model.vocabulary.IdentifierType;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class IdentifierTypeConverterTest {
+
+  @Test
+  public void testCompleteness() {
+    IdentifierTypeConverter conv = new IdentifierTypeConverter();
+
+    for (IdentifierType t : IdentifierType.values()) {
+      assertTrue(conv.fromEnum(t) >= 0);
+    }
+  }
+
+  @Test
+  public void testUnknown() {
+    IdentifierTypeConverter conv = new IdentifierTypeConverter();
+
+    assertEquals(IdentifierType.UNKNOWN, conv.toEnum(2008));
+    assertEquals(IdentifierType.UNKNOWN, conv.toEnum(-473289));
+    assertEquals(IdentifierType.UNKNOWN, conv.toEnum(null));
+
+  }
+}
---
+++ src/test/java/org/gbif/mybatis/type/IdentifierTypeTypeHandlerTest.java
@@ -0,0 +1,42 @@
+package org.gbif.mybatis.type;
+
+import org.gbif.api.exception.ServiceUnavailableException;
+import org.gbif.api.model.vocabulary.IdentifierType;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class IdentifierTypeTypeHandlerTest {
+
+  @Mock
+  private ResultSet mockedRs;
+  private IdentifierTypeTypeHandler th = new IdentifierTypeTypeHandler();
+
+  @Test
+  public void testGetResult() throws ServiceUnavailableException {
+    try {
+      when(mockedRs.getObject("type")).thenReturn(2007);
+      assertThat(th.getResult(mockedRs, "type"), is(IdentifierType.SOURCE_ID));
+    } catch (SQLException e) {
+    }
+  }
+
+  @Test
+  public void testGetNull() throws ServiceUnavailableException {
+    try {
+      when(mockedRs.getObject("type")).thenReturn(null);
+      assertThat(th.getResult(mockedRs, "type"), is(IdentifierType.UNKNOWN));
+    } catch (SQLException e) {
+    }
+  }
+}
