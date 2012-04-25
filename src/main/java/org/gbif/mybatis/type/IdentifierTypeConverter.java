e060f966c6999f5a3a0b3426609be8b3ec2cf29c
federicomh@gmail.com <federicomh@gmail.com@0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c>
2012-04-25 08:54:18 +0000

    IdentifierType changed: only 1 literal is enough for the DATA_PORTAL identifiers.
    git-svn-id: https://gbif-common-resources.googlecode.com/svn/gbif-common-mybatis/trunk@688 0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c

--- src/main/java/org/gbif/mybatis/type/IdentifierTypeConverter.java
+++ src/main/java/org/gbif/mybatis/type/IdentifierTypeConverter.java
@@ -6,10 +6,7 @@ import com.google.common.collect.ImmutableMap;
 
 import static org.gbif.api.model.vocabulary.IdentifierType.DOI;
 import static org.gbif.api.model.vocabulary.IdentifierType.FTP;
-import static org.gbif.api.model.vocabulary.IdentifierType.GBIF_PORTAL_DATA_PROVIDER_ID;
-import static org.gbif.api.model.vocabulary.IdentifierType.GBIF_PORTAL_DATA_RESOURCE_ID;
-import static org.gbif.api.model.vocabulary.IdentifierType.GBIF_PORTAL_NODE_ID;
-import static org.gbif.api.model.vocabulary.IdentifierType.GBIF_PORTAL_RESOURCE_ACCESS_POINT_ID;
+import static org.gbif.api.model.vocabulary.IdentifierType.GBIF_PORTAL;
 import static org.gbif.api.model.vocabulary.IdentifierType.HANDLER;
 import static org.gbif.api.model.vocabulary.IdentifierType.LSID;
 import static org.gbif.api.model.vocabulary.IdentifierType.SOURCE_ID;
@@ -27,7 +24,6 @@ public class IdentifierTypeConverter extends BaseConverter<Integer, IdentifierTy
     super(IdentifierType.UNKNOWN,
       new ImmutableMap.Builder<Integer, IdentifierType>().put(2000, URL).put(2001, LSID).put(2002, DOI)
         .put(2003, HANDLER).put(2004, UUID).put(2005, FTP).put(2006, URI).put(2007, SOURCE_ID).put(2008, UNKNOWN)
-        .put(2009, GBIF_PORTAL_DATA_RESOURCE_ID).put(2010, GBIF_PORTAL_DATA_PROVIDER_ID)
-        .put(2011, GBIF_PORTAL_RESOURCE_ACCESS_POINT_ID).put(2012, GBIF_PORTAL_NODE_ID).build());
+        .put(2009, GBIF_PORTAL).build());
   }
 }
