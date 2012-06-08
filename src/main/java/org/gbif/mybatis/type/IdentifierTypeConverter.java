42e1744b72c1700fc0feb2be5026676da7cb0fab
josecuadra <josecuadra@0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c>
2012-06-08 13:48:14 +0000

    adding a GBIF_NODE identifierType. This is used by the current live registry and we want to keep consistency.
    git-svn-id: https://gbif-common-resources.googlecode.com/svn/gbif-common-mybatis/trunk@755 0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c

--- src/main/java/org/gbif/mybatis/type/IdentifierTypeConverter.java
+++ src/main/java/org/gbif/mybatis/type/IdentifierTypeConverter.java
@@ -7,6 +7,7 @@ import com.google.common.collect.ImmutableMap;
 import static org.gbif.api.model.vocabulary.IdentifierType.DOI;
 import static org.gbif.api.model.vocabulary.IdentifierType.FTP;
 import static org.gbif.api.model.vocabulary.IdentifierType.GBIF_PORTAL;
+import static org.gbif.api.model.vocabulary.IdentifierType.GBIF_NODE;
 import static org.gbif.api.model.vocabulary.IdentifierType.HANDLER;
 import static org.gbif.api.model.vocabulary.IdentifierType.LSID;
 import static org.gbif.api.model.vocabulary.IdentifierType.SOURCE_ID;
@@ -24,6 +25,6 @@ public class IdentifierTypeConverter extends BaseConverter<Integer, IdentifierTy
     super(IdentifierType.UNKNOWN,
       new ImmutableMap.Builder<Integer, IdentifierType>().put(2000, URL).put(2001, LSID).put(2002, DOI)
         .put(2003, HANDLER).put(2004, UUID).put(2005, FTP).put(2006, URI).put(2007, SOURCE_ID).put(2008, UNKNOWN)
-        .put(2009, GBIF_PORTAL).build());
+        .put(2009, GBIF_PORTAL).put(2012, GBIF_NODE).build());
   }
 }
