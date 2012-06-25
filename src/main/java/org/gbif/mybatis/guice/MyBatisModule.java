3582dfcaff7dd4ad67a3a119428ce794f2f9afce
wixner <wixner@0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c>
2012-06-25 10:14:23 +0000

    Adding a new common mybatis module with bonecp and eh cache option
    git-svn-id: https://gbif-common-resources.googlecode.com/svn/gbif-common-mybatis/trunk@862 0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c

---
+++ src/main/java/org/gbif/mybatis/guice/MyBatisModule.java
@@ -0,0 +1,63 @@
+package org.gbif.mybatis.guice;
+
+import java.util.Properties;
+
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+import org.mybatis.guice.datasource.bonecp.BoneCPProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Basic mybatis module using BoneCP and offers a simple Property based option to turn on caching by setting
+ * cache=true.
+ */
+public abstract class MyBatisModule extends org.mybatis.guice.MyBatisModule {
+
+  protected final Properties properties;
+  private final String CACHE_KEY = "cache";
+  private static final Logger LOG = LoggerFactory.getLogger(MyBatisModule.class);
+
+  /**
+   * Properties with potential prefix scope already being removed.
+   */
+  public MyBatisModule(Properties properties) {
+    this.properties = properties;
+  }
+
+  @Override
+  protected void initialize() {
+    boolean useCache = false;
+    if (properties.containsKey(CACHE_KEY)) {
+      String x = properties.getProperty(CACHE_KEY);
+      try {
+        useCache = (x == null) ? false : (Boolean.parseBoolean(x));
+      } catch (Exception e) {
+        LOG.error("Bad configuration found for {}; {}", CACHE_KEY, x);
+      }
+    }
+    useCacheEnabled(useCache);
+    if (useCache) {
+      LOG.info("Configuring registry persitance with cache");
+      environmentId("production");
+    } else {
+      LOG.info("Configuring registry persitance with NO cache");
+      environmentId("development");
+    }
+
+    bindTransactionFactoryType(JdbcTransactionFactory.class);
+    bindMappers();
+    bindTypeHandlers();
+    bindManagers();
+
+    // TODO: Make configurable somehow? Load from classname property.
+    // Couldn't figure out how to do that with the generics though.
+    bindDataSourceProviderType(BoneCPProvider.class);
+  }
+
+  abstract void bindMappers();
+
+  abstract void bindTypeHandlers();
+
+  abstract void bindManagers();
+
+}
